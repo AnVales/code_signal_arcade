2c6adf5ee699e92bdf8a3174a1c30ad66e05f494
AnVales <a.vales@outlook.es>
2021-12-20 18:22:10 +0100

    add

---
+++ 4- Exploring the Waters/arrayChange.py
@@ -0,0 +1,19 @@
+# You are given an array of integers. 
+# On each move you are allowed to increase exactly one of its element by one. 
+# Find the minimal number of moves required to obtain a strictly increasing sequence from the input.
+
+inputArray = [1, 1, 1]
+
+def solution(inputArray):
+    prev = -float("inf")
+    steps = 0
+
+    for i in inputArray:
+        if i <= prev:
+            steps = steps + prev - i + 1
+            prev = prev - i + i + 1
+        else:
+            prev = i
+    return steps
+
+print(solution(inputArray))
