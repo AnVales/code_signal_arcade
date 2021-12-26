c32b04b4a2b1431b76bd1e4e67c30264054a1dd9
AnVales <a.vales@outlook.es>
2021-12-26 18:34:52 +0100

    Update reverseInParenthesis.py

--- 3 - Smooth Sailing/reverseInParenthesis.py
+++ 3 - Smooth Sailing/reverseInParenthesis.py
@@ -1,9 +1,20 @@
-lista = ['(perro)', '(gato)']
-inputString = 'El (perro) y el (gato)'
+# Write a function that reverses characters in (possibly nested) parentheses in the input string.
+
+# Input strings will always be well-formed with matching ()s.
 
 import re
 
-for i in lista:
-    inputString = re.sub(i, i[1:-1][::-1], inputString)
+def solution(inputString): 
+    # Patron que buscamos -> (letras)
+    pattern = '(\([a-z]*\))'
 
-print(inputString)
+    # Si hay patron:
+    if re.findall(pattern, inputString):
+        
+        # Reemplaza por el inverso sin patron
+        for i in re.findall(pattern, inputString):
+            inputString = inputString.replace(i, i[1:-1][::-1])
+        return solution(inputString)
+
+    else:
+        return inputString
