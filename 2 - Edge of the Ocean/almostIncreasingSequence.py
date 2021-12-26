25a8469a646b46f0d2c89ae40f1b1b7bbcafeef3
AnVales <a.vales@outlook.es>
2021-12-26 18:37:44 +0100

    Update almostIncreasingSequence.py

--- 2 - Edge of the Ocean/almostIncreasingSequence.py
+++ 2 - Edge of the Ocean/almostIncreasingSequence.py
@@ -1,8 +1,6 @@
 # Given a sequence of integers as an array, 
 # determine whether it is possible to obtain a strictly increasing sequence by removing no more than one element from the array.
 
-array_1 = [1, 2, 1, 2]
-
 def almostIncreasingSequence(sequence):
 
     counter = 0
