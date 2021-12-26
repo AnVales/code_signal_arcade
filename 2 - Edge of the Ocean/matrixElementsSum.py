fd259b9e99583070a097a719f0e9dd79f8e49aed
AnVales <a.vales@outlook.es>
2021-12-26 18:38:25 +0100

    Update matrixElementsSum.py

--- 2 - Edge of the Ocean/matrixElementsSum.py
+++ 2 - Edge of the Ocean/matrixElementsSum.py
@@ -9,21 +9,19 @@
 # your task is to return the total sum of all rooms that are suitable for the CodeBots 
 # (ie: add up all the values that don't appear below a 0).
 
-matrix_a = [[0,1,1,2], 
-            [0,5,0,0], 
-            [2,0,3,3]]
+def solution(matrix_a):
 
-matrix_b = matrix_a
-value = 0
+    matrix_b = matrix_a
+    value = 0
 
-for i in range(len(matrix_a)):
-    for j in range(len(matrix_a[0])):
-        if matrix_a[i][j] == 0 and i+1<len(matrix_a):
-            matrix_b[i+1][j] = 0
+    for i in range(len(matrix_a)):
+        for j in range(len(matrix_a[0])):
+            if matrix_a[i][j] == 0 and i+1<len(matrix_a):
+                matrix_b[i+1][j] = 0
 
-for i in range(len(matrix_b)):
-    for j in range(len(matrix_b[0])):
-        value = matrix_a[i][j] + value
+    for i in range(len(matrix_b)):
+        for j in range(len(matrix_b[0])):
+            value = matrix_a[i][j] + value
 
-print(value)
+    return value
 
