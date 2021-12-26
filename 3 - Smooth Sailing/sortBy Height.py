5112a9aca06d8d2f992f902538ce89541a7a6344
AnVales <a.vales@outlook.es>
2021-12-26 18:35:54 +0100

    Update sortBy Height.py

--- 3 - Smooth Sailing/sortBy Height.py
+++ 3 - Smooth Sailing/sortBy Height.py
@@ -3,21 +3,20 @@
 # Your task is to rearrange the people by their heights in a non-descending order without moving the trees. 
 # People can be very tall!
 
-a = [-1, 150, 190, 170, -1, -1, 160, 180]
+def solution(a):
 
-a_sorted = sorted(a)
-a_sorted = [i for i in a_sorted if i != -1]
-print(a_sorted)
+    a_sorted = sorted(a)
+    a_sorted = [i for i in a_sorted if i != -1]
 
-a_final = []
-counter = 0
+    a_final = []
+    counter = 0
 
-for i in range(len(a)):
-    if a[i] == -1:
-        a_final.append(a[i])
-        counter = counter - 1
+    for i in range(len(a)):
+        if a[i] == -1:
+            a_final.append(a[i])
+            counter = counter - 1
 
-    else:
-        a_final.append(a_sorted[i+counter])
-
-print(a_final)
+        else:
+            a_final.append(a_sorted[i+counter])
+            
+    return a_final
