b901798612ebf13db6c9e4bcc2aeba6fb1490e77
AnVales <a.vales@outlook.es>
2021-12-26 18:30:20 +0100

    eliminate_example

--- 7 -Through the Fog/depositProfit.py
+++ 7 -Through the Fog/depositProfit.py
@@ -2,15 +2,9 @@
 # Each year your balance increases at the same growth rate. 
 # With the assumption that you don't make any additional deposits, find out how long it would take for your balance to pass a specific threshold.
 
-deposit = 100
-rate = 20
-threshold = 170
-
 def solution(deposit, rate, threshold):
 
     for times in range(1000):
         deposit = deposit * (1 + (rate/100))
         if deposit >= threshold:
             return(times+1)
-
-print(solution(deposit, rate, threshold))
