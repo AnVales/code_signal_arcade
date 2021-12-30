dc5f1faf4c9ff02e1b0e7062b0d296fcf2a490d9
AnVales <a.vales@outlook.es>
2021-12-30 12:54:20 +0100

    Add files via upload

---
+++ 9 - Dark Wilderness/growingPlant.py
@@ -0,0 +1,40 @@
+# Caring for a plant can be hard work, but since you tend to it regularly, 
+# you have a plant that grows consistently. 
+# Each day, its height increases by a fixed amount represented by the integer upSpeed. 
+# But due to lack of sunlight, the plant decreases in height every night, by an amount represented by downSpeed.
+
+# Since you grew the plant from a seed, it started at height 0 initially. 
+# Given an integer desiredHeight, your task is to find how many days it'll take for the plant to reach this height.
+
+# Day growth function
+def day(height, upSpeed):
+    height = height + upSpeed
+    return height
+
+# Night growth function
+def night(height, downSpeed):
+    height = height - downSpeed
+    return height
+
+# Function that shows when the plant grows on the desired day.
+def solution(upSpeed, downSpeed, desiredHeight):
+    
+    # Initialise variable
+    height = 0
+    time = 10000
+
+    for i in range(time):
+        
+        # The plant grows by day
+        height = day(height, upSpeed)
+
+        # Enough height?
+        if height >= desiredHeight:
+            return(i+1)
+
+        # The plant declines at night
+        height = night(height, downSpeed)
+
+        # Enough height?
+        if height >= desiredHeight:
+            return(i+1)
