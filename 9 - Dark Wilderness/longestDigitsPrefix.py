fdb956a392e615ab8b42cb08dc7ffa22f8dc9926
AnVales <a.vales@outlook.es>
2022-01-07 13:02:17 +0100

    end_chess_edit

--- 9 - Dark Wilderness/longestDigitsPrefix.py
+++ 9 - Dark Wilderness/longestDigitsPrefix.py
@@ -1,8 +1,10 @@
 # Given a string, output its longest prefix which contains only digits.
 
+# Solution function
 def solution(inputString):
     import re
 
+    # Search the first digits
     output = re.search('^\d*', inputString)
 
     return(output.group())
