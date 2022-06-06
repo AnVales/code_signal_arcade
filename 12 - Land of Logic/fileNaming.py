e3fd01ce9b9834946603c9e0575030257c02ad08
AnVales <a.vales@outlook.es>
2022-06-06 12:35:23 +0200

    binary

--- 12 - Land of Logic/fileNaming.py
+++ 12 - Land of Logic/fileNaming.py
@@ -5,3 +5,27 @@
 
 # Return an array of names that will be given to the files.
 
+def solution(names):
+
+    # Initialize 
+    names_final = []
+
+    # Go through the initial list
+    # If there is no identical one on the final list, it adds it
+    for name in names:
+        if not name in names_final: 
+            names_final.append(name)
+
+        # If it is already there, we look for the following: name(frec)
+        # Frec = 1
+        # As long as there is a match, adds 1 to the frequency
+        # When there are no more matches, it appends it with the final frequency
+        else:
+            n = 1
+            while "{0}({1})".format(name, n) in names_final:
+                n = n + 1
+
+            names_final.append(name+"("+str(n)+")")
+
+    return (names_final)
+
--- 12 - Land of Logic/validTime.py
+++ 12 - Land of Logic/validTime.py
@@ -1,16 +1,12 @@
 # Check if the given string is a correct time representation of the 24-hour clock.
 
-time = "13:58"
-
 import re
 
 def solution(time):
 
     times = re.findall(r'[0-9]+', time)
     
-    if not 0<=int(times[0])<23 or not 0<=int(times[1])<61:
+    if not 0<=int(times[0])<=23 or not 0<=int(times[1])<60:
         return False
     
     return True
-
-print(solution(time))
